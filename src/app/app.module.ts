1a9a01393f5fc939deb5fd0bbee2823f3ed7105d
Ivan Flores <emmanuel.flores.dev84@gmail.com>
2024-09-13 14:48:18 -0600

    se sube proyecto inicializado con primer version de un servicio por capital

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from "./shared/shared.module";
+import { AppRoutingModule } from './app-routing.module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    SharedModule,
+    AppRoutingModule,
+],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
